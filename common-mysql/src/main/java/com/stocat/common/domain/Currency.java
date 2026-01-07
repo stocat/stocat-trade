0dd523ca6f8d0562b86f71a3950709036e162f5d
seminchoi <choicco89@gmail.com>
2026-01-07 19:14:47 +0900

    refactor: Currency 거래 카테고리 enum을 필드로 추가

--- common-mysql/src/main/java/com/stocat/common/domain/Currency.java
+++ common-mysql/src/main/java/com/stocat/common/domain/Currency.java
@@ -13,6 +13,10 @@ public enum Currency {
         this.category = category;
     }
 
+    /**
+     * Currency에 해당하는 자산 카테고리를 반환합니다.
+     * Asset을 거래할 때 어떤 자산(현금) 카테고리를 예약하거나 수정해야하는 지 알기 위해 사용하는 메소드입니다.
+     */
     public AssetsCategory getCategory() {
         if (category == null) {
             throw new IllegalStateException("해당 통화에 매칭되는 자산 카테고리가 없습니다.");
