a7b598ce0bf4f753822cc26cd147a48de5cdc44f
xEzIxX <by1094@gachon.ac.kr>
2026-01-24 23:20:42 +0900

    fix: @Positive 동작을 위해 클래스 레벨에 @Validated 추가

--- trade-api/src/main/java/com/stocat/tradeapi/position/controller/PositionController.java
+++ trade-api/src/main/java/com/stocat/tradeapi/position/controller/PositionController.java
@@ -11,11 +11,18 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @Tag(name = "Position", description = "포지션 조회 API")
 @RestController()
 @RequestMapping("/positions")
