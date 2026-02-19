23110a2508fbe7756ccc21cab9c36fb77fbaf11b
xEzIxX <by1094@gachon.ac.kr>
2026-02-19 22:25:31 +0900

    fix: BuyOrderUsecase 외부 API 호출 시 DB 락 점유 가능성 문제 해결

--- trade-api/src/main/java/com/stocat/tradeapi/order/usecase/BuyOrderUsecase.java
+++ trade-api/src/main/java/com/stocat/tradeapi/order/usecase/BuyOrderUsecase.java
@@ -1,35 +1,25 @@
 package com.stocat.tradeapi.order.usecase;
 
-import com.stocat.common.domain.cash.CashHoldingEntity;
 import com.stocat.common.exception.ApiException;
-import com.stocat.tradeapi.cash.service.CashService;
-import com.stocat.tradeapi.cash.service.dto.command.CreateCashHoldingCommand;
 import com.stocat.tradeapi.exception.TradeErrorCode;
 import com.stocat.tradeapi.infrastructure.quoteapi.QuoteApiClient;
 import com.stocat.tradeapi.infrastructure.quoteapi.dto.AssetDto;
-import com.stocat.tradeapi.order.service.OrderService;
 import com.stocat.tradeapi.order.service.dto.OrderDto;
 import com.stocat.tradeapi.order.service.dto.command.BuyOrderCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class BuyOrderUsecase {
-    private final OrderService orderService;
     private final QuoteApiClient quoteApiClient;
-    private final CashService cashService;
+    private final BuyOrderFacade buyOrderFacade;
 
-    @Transactional
     public OrderDto placeBuyOrder(BuyOrderCommand command) {
         AssetDto asset = quoteApiClient.fetchAsset(command.assetSymbol());
         validateAsset(asset);
 
-        CashHoldingEntity holding = holdCash(asset, command);
-        OrderDto order = orderService.placeBuyOrder(command, asset, holding.getId());
-
-        return order;
+        return buyOrderFacade.processBuyOrder(command, asset);
     }
 
     private void validateAsset(AssetDto asset) {
@@ -39,17 +29,5 @@ public class BuyOrderUsecase {
         if (!asset.isDaily()) {
             throw new ApiException(TradeErrorCode.NOT_DAILY_PICK_ASSET);
         }
-    }
-
-    private CashHoldingEntity holdCash(AssetDto asset, BuyOrderCommand command) {
-        if (asset.currency() == null || command.price() == null) {
-            throw new ApiException(TradeErrorCode.INTERNAL_ERROR);
-        }
-        CreateCashHoldingCommand holdingCommand = new CreateCashHoldingCommand(
-                command.userId(),
-                asset.currency(),
-                command.price().multiply(command.quantity())
-        );
-        return cashService.createCashHolding(holdingCommand);
     }
 }
