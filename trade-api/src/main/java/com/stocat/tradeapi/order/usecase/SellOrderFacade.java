9051af434244c65a787fc59ab4d89c63cdfa362c
xEzIxX <by1094@gachon.ac.kr>
2026-03-02 00:14:56 +0900

    feat: 매도 주문 시 포지션 예약 기능 구현
    - PositionEntity 에 예약 수량 필드 추가
    - SellOrderFacade 에서 매도 주문 생성 시 포지션 예약 처리 추가

--- trade-api/src/main/java/com/stocat/tradeapi/order/usecase/SellOrderFacade.java
+++ trade-api/src/main/java/com/stocat/tradeapi/order/usecase/SellOrderFacade.java
@@ -9,6 +9,7 @@ import com.stocat.tradeapi.order.service.OrderCommandService;
 import com.stocat.tradeapi.order.service.dto.OrderDto;
 import com.stocat.tradeapi.order.service.dto.command.SellOrderCommand;
 import com.stocat.tradeapi.position.service.PositionQueryService;
+import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,19 +22,22 @@ public class SellOrderFacade {
 
     @Transactional
     public OrderDto processSellOrder(SellOrderCommand command, AssetDto asset) {
-        validatePosition(command, asset);
+        reservePosition(command, asset);
 
         Order order = orderCommandService.createSellOrder(command, asset);
         return OrderDto.from(order);
     }
 
-    private void validatePosition(SellOrderCommand command, AssetDto asset) {
+    private void reservePosition(SellOrderCommand command, AssetDto asset) {
         PositionEntity position = positionQueryService
                 .getUserPositionForUpdate(asset.id(), command.userId())
                 .orElseThrow(() -> new ApiException(TradeErrorCode.POSITION_NOT_FOUND_FOR_SELL));
 
-        if (position.getQuantity().compareTo(command.quantity()) < 0) {
+        BigDecimal availableQuantity = position.getQuantity().subtract(position.getReservedQuantity());
+        if (availableQuantity.compareTo(command.quantity()) < 0) {
             throw new ApiException(TradeErrorCode.INSUFFICIENT_POSITION_QUANTITY);
         }
+
+        position.reserve(command.quantity());
     }
 }
